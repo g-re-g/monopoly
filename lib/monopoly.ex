c38abf6e45bd7fd40a7b44eb0e5fac3bbba9846b
greg <greg@greg.lol>
2023-09-06 22:54:22 -0400

    Let there be code

---
+++ lib/monopoly.ex
@@ -0,0 +1,18 @@
+defmodule Monopoly do
+  @moduledoc """
+  Documentation for `Monopoly`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Monopoly.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/monopoly_test.exs
@@ -0,0 +1,8 @@
+defmodule MonopolyTest do
+  use ExUnit.Case
+  doctest Monopoly
+
+  test "greets the world" do
+    assert Monopoly.hello() == :world
+  end
+end
